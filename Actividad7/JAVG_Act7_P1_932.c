dd333ab311913613ae4147b3045e43f767666216
JVPresti <nanojorgevazquez123@gmail.com>
2023-09-24 19:05:03 -0700

    Parte1

--- Actividad7/JAVG_Act7_P1_932.c
+++ Actividad7/JAVG_Act7_P1_932.c
@@ -6,8 +6,10 @@
 #include <stdlib.h>
 #include <string.h>
 
+//Declaracion de los prototipos
 int largo_cadena(char cadena[]);
 void menu(void);
+void convmayus(char cadena[]);
 void sali1(char cadena[]);
 void sali2(char cadena[]);
 void sali3(char cadena[]);
@@ -17,15 +19,19 @@ void sali6(char cadena[]);
 void sali7(char cadena[]);
 void sali8(char cadena[]);
 void sali9(char cadena[]);
+void sali10(char cadena[]);
 
-
-int main(){
+//Funcion main que llama al menu
+int main()
+{
     menu();
 
     return 0;
 }
 
-void menu(){
+//Funcion que da al usuario la opcion de elegir alguna salida
+void menu()
+{
     int op;
     char cadena[30];
 
@@ -39,123 +45,228 @@ void menu(){
     printf("7. Salida 7\n");
     printf("8. Salida 8\n");
     printf("9. Salida 9\n");
+    printf("10. Salida 10\n");
     scanf("%d", &op);
     printf("\n Ingresa una cadena de caracteres: ");
     fflush(stdin);
     gets(cadena);
+    convmayus(cadena);
 
-    switch(op){
-        case 1:
-            sali1(cadena);
+    switch (op)
+    {
+    case 1:
+        sali1(cadena);
         break;
-        case 2:
-            sali2(cadena);
+    case 2:
+        sali2(cadena);
         break;
-        case 3:
-            sali3(cadena);
+    case 3:
+        sali3(cadena);
         break;
-        case 4:
-            sali4(cadena);
+    case 4:
+        sali4(cadena);
         break;
-        case 5:
-            sali5(cadena);
+    case 5:
+        sali5(cadena);
         break;
-        case 6:
-            sali6(cadena);
+    case 6:
+        sali6(cadena);
         break;
-        case 7:
-            sali7(cadena);
+    case 7:
+        sali7(cadena);
         break;
-        case 8:
-            sali8(cadena);
+    case 8:
+        sali8(cadena);
         break;
-        case 9:
-            sali9(cadena);
+    case 9:
+        sali9(cadena);
         break;
-        default:
-            printf("\n Opcion no valida");
+    case 10:
+        sali10(cadena);
+        break;
+    default:
+        printf("\n Opcion no valida");
     }
 }
 
-int largo_cadena(char cadena[]){
+//Funcion que cuando es llamada cuanta el largo de la cadena
+int largo_cadena(char cadena[])
+{
     int i;
-    i=0;
-    while(cadena[i]!='\0'){
+    i = 0;
+    while (cadena[i] != '\0')
+    {
         i++;
     }
     return i;
 }
 
-void sali1(char cadena[]){
+//Funcion que cuando es llamada convierte todo a mayusculas
+void convmayus(char cadena[])
+{
+    int i;
+    char caracter;
+
+    for (i = 0; cadena[i] != '\0'; i++)
+    {
+        caracter = cadena[i];
+
+        if (caracter >= 'a' && caracter <= 'z')
+        {
+            caracter -= 32;
+        }
+
+        cadena[i] = caracter;
+    }
+}
+
+//Funcion que cuando es llamada imprime la cadena normal
+void sali1(char cadena[])
+{
     printf("%s", cadena);
 }
 
-void sali2(char cadena[]){
+//Funcion que cuando es llamada imprime la cadena al reves
+void sali2(char cadena[])
+{
     char cadena2[30];
     int i, j;
-    j= largo_cadena(cadena);
+    j = largo_cadena(cadena);
 
-    for(i=0; i<j; i++){
-        cadena2[j-i-1]=cadena[i];
+    for (i = 0; i < j; i++)
+    {
+        cadena2[j - i - 1] = cadena[i];
     }
 
     printf("%s", cadena2);
 }
 
-void sali3(char cadena[]){
+//Funcion que cuando es llamada imprime la cadena en forma de lista
+void sali3(char cadena[])
+{
     printf("\n%s", cadena);
 }
 
-void sali4(char cadena[]){
+//Funcion que cuando es llamada imprime la cadena al reves en forma de lista
+void sali4(char cadena[])
+{
     char cadena2[30];
     int i, j;
-    j= largo_cadena(cadena);
+    j = largo_cadena(cadena);
 
-    for(i=0; i<j; i++){
-        cadena2[j-i-1]=cadena[i];
+    for (i = 0; i < j; i++)
+    {
+        cadena2[j - i - 1] = cadena[i];
     }
 
     printf("\n%s", cadena2);
 }
 
-void sali5(char cadena[]){
-    int i, n; 
+//Funcion que cuando es llamada imprime la cadena normal y le va eliminando caracteres a la derecha
+void sali5(char cadena[])
+{
+    int i, n;
 
-    n= largo_cadena(cadena);
+    n = largo_cadena(cadena);
     printf("%s", cadena);
-    for(i=n-1; i>=0; i--){
-        cadena[i]='\0';
+    for (i = n - 1; i >= 0; i--)
+    {
+        cadena[i] = '\0';
         printf("\n%s", cadena);
-    } 
+    }
 }
 
-void sali6(char cadena[]){
+//Funcion que cuando es llamada imprime la cadena al reves y le va eliminando caracteres a la derecha
+void sali6(char cadena[])
+{
     char cadena2[30];
     int i, n;
-    n= largo_cadena(cadena);
+    n = largo_cadena(cadena);
 
-    for(i=0; i<n; i++){
-        cadena2[n-i-1]=cadena[i];
+    for (i = 0; i < n; i++)
+    {
+        cadena2[n - i - 1] = cadena[i];
     }
     printf("%s", cadena2);
-    for(i=n-1; i>=0; i--){
-        cadena2[i]='\0';
+    for (i = n - 1; i >= 0; i--)
+    {
+        cadena2[i] = '\0';
         printf("\n%s", cadena2);
-    } 
+    }
 }
 
-void sali7(char cadena[]){
-    int i, n; 
+//Funcion que cuando es llamada imprime la cadena y le va eliminando caracteres a la izquierda
+void sali7(char cadena[])
+{
+    int i, j, n;
 
-    n= largo_cadena(cadena);
-    printf("%s", cadena);
-    for(i=0; i<=n; i--){
-        cadena[i]='\0';
-        printf("\n%s", cadena);
-    } 
-    printf("%d", i);//quitar esta linea
+    n = largo_cadena(cadena);
+
+    for (i = n; i >= 0; i--)
+    {
+        for (j = n - i; j <= n; j++)
+        {
+            printf("%c", cadena[j]);
+        }
+        printf("\n");
+    }
 }
 
+//Funcion que cuando es llamada imprime la cadena al reves y le va eliminando caracteres a la izquierda
+void sali8(char cadena[])
+{
+    int i, j, n;
 
+    n = largo_cadena(cadena);
 
-//Tengo que terminar este codigo
+    for (i = n; i >= 0; i--)
+    {
+        for (j = i; j >= 0; j--)
+        {
+            printf("%c", cadena[j]);
+        }
+        printf("\n");
+    }
+}
+
+//Funcion que cuando es llamada imprime las consonantes de la cadena
+void sali9(char cadena[])
+{
+    int i;
+
+    for (i = 0; cadena[i] != '\0'; i++)
+    {
+        char caracter = cadena[i];
+
+        if (caracter >= 'A' && caracter <= 'Z')
+        {
+            caracter += 32;
+        }
+
+        if ((caracter >= 'a' && caracter <= 'z') && (caracter != 'a' && caracter != 'e' && caracter != 'i' && caracter != 'o' && caracter != 'u'))
+        {
+            printf("%c", cadena[i]);
+        }
+    }
+}
+
+//Funcion que cuando es llamada imprime las vocales de la cadena
+void sali10(char cadena[])
+{
+    int i;
+
+    for (i = 0; cadena[i] != '\0'; i++)
+    {
+        char caracter = cadena[i];
+
+        if (caracter >= 'A' && caracter <= 'Z')
+        {
+            caracter += 32;
+        }
+
+        if ((caracter >= 'a' && caracter <= 'z') && (caracter == 'a' || caracter == 'e' || caracter == 'i' || caracter == 'o' || caracter == 'u'))
+        {
+            printf("%c", cadena[i]);
+        }
+    }
+}
